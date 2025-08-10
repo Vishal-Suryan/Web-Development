ece4efdce3c8cd55896fdebc9cef8d805a7a6d95
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-08-10 15:40:43 +0530

    node started

---
+++ Node js/app.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+const math = require("./math");
+const { sub } = require("./math");
+console.log("Math value is", math.add(5, 7));
+console.log("Math value is", math.sub(5, 7));
