aea3db6504f4ef9bb5174a24b04be7661e1106c1
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2025-08-21 19:28:20 +0530

    commit auth with jwt

--- Node js/Url Shortner/service/auth.js
+++ Node js/Url Shortner/service/auth.js
@@ -1,11 +1,30 @@
+const jwt = require("jsonwebtoken");
+const secret = "hgsdvjyfcsvcg1876rt36@!!U*(@Y&*(@";
 const sessionIdToUserMap = new Map();
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
+// function setUser(id, user) {
+//   sessionIdToUserMap.set(id, user);
+// }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
+// function getUser(id) {
+//   return sessionIdToUserMap.get(id);
+// }
 
 module.exports = {
   setUser,
