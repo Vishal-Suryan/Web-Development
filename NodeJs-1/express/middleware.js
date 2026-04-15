383826589429eebe9f9090ea8edc0eb4737b5de1
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2026-04-16 01:03:37 +0530

    express
    express, middleware, routes

---
+++ NodeJs-1/express/middleware.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+//define middleware function
+const myFirstMiddleware = (req, res, next) => {
+  console.log("this first middleware will run on every request");
+  next();
+};
+
+app.use(myFirstMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Server running on port 3000");
+});
---
+++ NodeJs-1/express/routes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+//root route
+app.get("/", (req, res) => {
+  res.send("Welcome to our Home Page");
+});
+
+//get all products
+app.get("/products", (req, res) => {
+  const products = [
+    { id: 1, label: "Product 1" },
+    { id: 2, label: "Product 2" },
+    { id: 3, label: "Product 3" },
+  ];
+  res.json(products);
+});
+
+//get a single product
+app.get("/products/:id", (req, res) => {
+  const products = [
+    { id: 1, label: "Product 1" },
+    { id: 2, label: "Product 2" },
+    { id: 3, label: "Product 3" },
+  ];
+  const productId = parseInt(req.params.id);
+  const getSingleProduct = products.find((product) => product.id === productId);
+  if (getSingleProduct) {
+    res.json(getSingleProduct);
+  } else {
+    res.status(404).send("product not found");
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
