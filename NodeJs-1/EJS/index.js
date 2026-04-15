cfda006651778029f6fb3727f0bb6e59512b00e7
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2026-04-16 01:31:27 +0530

    Ejs

---
+++ NodeJs-1/EJS/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+//set the view engine as ejs
+app.set("view engine", "ejs");
+
+//set the directory for the views
+app.set("views", path.join(__dirname, "views"));
+
+const products = [
+  { id: 1, label: "Product 1" },
+  { id: 2, label: "Product 2" },
+  { id: 3, label: "Product 3" },
+];
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home", products: products });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About page" });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Server running on port 3000");
+});
