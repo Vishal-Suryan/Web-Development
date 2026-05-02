f489fbf7e4492014f93de58680cc18caa81d8dd2
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2026-05-02 23:20:25 +0530

    Update book-controllers.js
    added add book and get all book routes methods

--- NodeJs-1/6.bookstore-api/controllers/book-controllers.js
+++ NodeJs-1/6.bookstore-api/controllers/book-controllers.js
@@ -1,6 +1,48 @@
-const getAllBooks = async (req, res) => {};
+const Book = require("../models/Book");
+
+const getAllBooks = async (req, res) => {
+  try {
+    const allBooks = await Book.find({});
+    if (allBooks.length > 0) {
+      res.status(200).json({
+        success: true,
+        message: "List of books fetched successfully",
+        data: allBooks,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "No Books found",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong!",
+    });
+  }
+};
 const getSingleBook = async (req, res) => {};
-const addNewBook = async (req, res) => {};
+const addNewBook = async (req, res) => {
+  try {
+    const newBookFormData = req.body;
+    const newlyCreatedBook = await Book.create(newBookFormData);
+    if (newBookFormData) {
+      res.status(201).json({
+        success: true,
+        message: "Book added",
+        data: newlyCreatedBook,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong!",
+    });
+  }
+};
 const updateSingleBook = async (req, res) => {};
 const deleteBook = async (req, res) => {};
 
