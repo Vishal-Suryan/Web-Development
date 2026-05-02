6f658dfaa0827b8d203e0e9a40e696361f594d57
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2026-05-03 01:33:29 +0530

    Update book-controllers.js
    all routes completed

--- NodeJs-1/6.bookstore-api/controllers/book-controllers.js
+++ NodeJs-1/6.bookstore-api/controllers/book-controllers.js
@@ -23,7 +23,29 @@ const getAllBooks = async (req, res) => {
     });
   }
 };
-const getSingleBook = async (req, res) => {};
+const getSingleBook = async (req, res) => {
+  try {
+    const bookDetailsById = await Book.findById(req.params.id);
+    if (bookDetailsById) {
+      res.status(200).json({
+        success: true,
+        message: "Book fetched successfully",
+        data: bookDetailsById,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "No Book found with this ID",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong!",
+    });
+  }
+};
 const addNewBook = async (req, res) => {
   try {
     const newBookFormData = req.body;
@@ -43,8 +65,56 @@ const addNewBook = async (req, res) => {
     });
   }
 };
-const updateSingleBook = async (req, res) => {};
-const deleteBook = async (req, res) => {};
+const updateSingleBook = async (req, res) => {
+  try {
+    const formData = req.body;
+    const getCurrId = req.params.id;
+    const bookDetailsById = await Book.findByIdAndUpdate(getCurrId, formData, {
+      new: true,
+    });
+    if (bookDetailsById) {
+      res.status(200).json({
+        success: true,
+        message: "Book updated successfully",
+        data: bookDetailsById,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "No Book found with this ID",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong!",
+    });
+  }
+};
+const deleteBook = async (req, res) => {
+  try {
+    const getCurrentBookById = await Book.findByIdAndDelete(req.params.id);
+    if (getCurrentBookById) {
+      res.status(200).json({
+        success: true,
+        message: "Book deleted successfully",
+        data: getCurrentBookById,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "No Book found with this ID",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong!",
+    });
+  }
+};
 
 module.exports = {
   getAllBooks,
