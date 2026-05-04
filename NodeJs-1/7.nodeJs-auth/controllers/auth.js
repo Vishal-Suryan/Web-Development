6d26d580246355ec992f9fb5e60130e481694c9a
Vishal Suryan <115346638+Vishal-Suryan@users.noreply.github.com>
2026-05-04 15:44:18 +0530

    authentication and authorization
    added role based access

--- NodeJs-1/7.nodeJs-auth/controllers/auth.js
+++ NodeJs-1/7.nodeJs-auth/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 //register controller
 const registerUser = async (req, res) => {
   try {
@@ -48,11 +49,43 @@ const registerUser = async (req, res) => {
 //login controller
 const loginUser = async (req, res) => {
   try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User doesn't exsist",
+      });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "invalid username or password",
+      });
+    }
+    //create a user token
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+        username: user.username,
+        role: user.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "15m",
+      },
+    );
+    res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      accessToken,
+    });
   } catch (e) {
     console.log(e);
     res.status(500).json({
       success: false,
-      message: "Register Failed, please try again",
+      message: "Login Failed, please try again",
     });
   }
 };
---
+++ NodeJs-1/7.nodeJs-auth/middlewares/auth-middleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const authMiddleware = (req, res, next) => {
+  //   console.log("Auth Middleware called");
+  const authHeader = req.headers["authorization"];
+  console.log(authHeader);
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Access denied.Please login to continue",
+    });
+  }
+  //decode token
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    console.log(decodedToken);
+    req.userInfo = decodedToken;
+    next();
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      message: "Access denied.Please login to continue",
+    });
+  }
+};
+
+module.exports = authMiddleware;
